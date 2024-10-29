35625f6d826f2083ec51df85ccb5da4f46b913fb
Bartek <bartosz_janusz731@wp.pl>
2024-10-29 13:04:15 +0100

    Initial commit with mysql basic onfig

---
+++ backend/RideTheSchedule/src/main/java/axan18/ridetheschedule/RideTheScheduleApplication.java
@@ -0,0 +1,13 @@
+package axan18.ridetheschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideTheScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideTheScheduleApplication.class, args);
+    }
+
+}
---
+++ backend/RideTheSchedule/src/test/java/axan18/ridetheschedule/RideTheScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package axan18.ridetheschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideTheScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
